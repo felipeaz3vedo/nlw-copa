4828edf3d0dfb6b80762952bfc8da8cfeba0a773
felipeaz3vedo <felipedeazevedopessoa@gmail.com>
2022-11-12 21:52:28 -0300

    Next setup created

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,10 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({
+  log: ["query"]
+});
 
 async function bootstrap() {
   const fastify = Fastify({
@@ -10,11 +15,13 @@ async function bootstrap() {
     origin: true
   });
 
-  fastify.get("/pools/count", () => {
-    return { count: 0 };
+  fastify.get("/pools/count", async () => {
+    const count = await prisma.pool.count();
+
+    return { count };
   });
 
-  await fastify.listen({ port: 3333, host: '0.0.0.0' });
+  await fastify.listen({ port: 3333, /*host: "0.0.0.0"*/ });
 }
 
 bootstrap();
